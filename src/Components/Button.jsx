4173a6e973c03d2cd2d08605f070dd71d79203b5
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-08-25 20:09:52 +0500

    Add Styling.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text, onClick }) => {
     return (
-        <button onClick={onClick} className='rounded-2xl bg-green-950 text-white p-3 mt-2 font-bold'>
+        <button onClick={onClick} className=' rounded-[5px] bg-green-950 text-white p-3 mt-2 font-bold m-1'>
             {text}
         </button>
     )
